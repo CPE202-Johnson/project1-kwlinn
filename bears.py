0c7ccd471d40bccebef049a8e6bef5193b9ddfe2
kwlinn <kwlin@calpoly.edu>
2021-01-14 22:11:50 -0800

    added test case

--- bears.py
+++ bears.py
@@ -10,18 +10,18 @@ def bears(n):
 
     if n == 42:
         return True
-    if n<= 0:
+    if n <= 0:
         return False
-    if n%2 == 0: #if even, give back n/2 bears
-        div_2 = bears(n/2)
-    if ((n%3 == 0) or (n%4 == 0)) and div_2 == False:
+    if n % 2 == 0: #if even, give back n/2 bears
+        div_2 = bears(n / 2)
+    if ((n % 3 == 0) or (n % 4 == 0)) and div_2 == False:
         # If it wasn't divisible by 2 but it is divisible by 3 or 4,
-        last = n%10 
-        second_to_last = n//10%10
-        if last*second_to_last != 0:
-            div_3_4 = bears(n - (last*second_to_last))
+        last = n % 10 
+        second_to_last = n // 10 % 10
+        if last * second_to_last != 0:
+            div_3_4 = bears(n - (last * second_to_last))
         # div_3_4 is true if it is possible to win with that path
-    if (n%5 == 0) and div_2 == False and div_3_4 == False:
+    if (n % 5 == 0) and div_2 == False and div_3_4 == False:
         div_5 = bears(n - 42)
     
     if div_2 or div_3_4 or div_5:
