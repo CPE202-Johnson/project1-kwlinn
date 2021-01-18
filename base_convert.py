370389aebb298cb3bfe78d2a6283b0d34dab7fad
kwlinn <kwlin@calpoly.edu>
2021-01-17 21:45:00 -0800

    added comments

--- base_convert.py
+++ base_convert.py
@@ -1,8 +1,9 @@
 #integer -> string
+#returns a string representing var num in the base b
 
 def convert(num, b):
     """Recursive function that returns a string representing num in the base b"""
-    convert_string = "0123456789ABCDEF"
+    convert_string = "0123456789ABCDEF" 
     if num < b:
         return convert_string[num]
     return convert(num // b, b) + convert_string[num % b]
